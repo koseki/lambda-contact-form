b0ac08f5be53fbc12f8b01c08438183084f12d79
KOSEKI Kengo <koseki@gmail.com>
2019-07-26 19:28:35 +0900

    Add config file for sample HTML

---
+++ static/samples/config-sample.js
@@ -0,0 +1,7 @@
+var apiMethod = 'POST';
+var apiURL = '/post';
+// var apiurl = 'https://xxxxxxxx.execute-api.ap-northeast-1.amazonaws.com/Prod/post';
+
+// var apiMethod = 'GET';
+// var apiURL = '/samples/validated.json';
+// var apiURL = '/samples/validation.json';
